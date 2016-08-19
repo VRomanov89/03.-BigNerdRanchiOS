65fa2446890754646ce772a61e7a8f42cefcc728
Vladimir Romanov <vladromweb@gmail.com>
2016-08-19 15:50:16 -0700

    embedded in uinavigation controller and fixed delegate crash
    embedded in uinavigation controller and fixed delegate crash

--- HomeBNR App/HomeBNR App/DetailViewController.swift
+++ HomeBNR App/HomeBNR App/DetailViewController.swift
@@ -9,5 +9,33 @@
 import UIKit
 
 class DetailViewController: UIViewController {
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var serialTextField: UITextField!
+    @IBOutlet weak var valueTextField: UITextField!
+    @IBOutlet weak var dateLabel: UILabel!
     
+    var item: Item!
+    
+    let numberFormatter: NSNumberFormatter = {
+        let formatter = NSNumberFormatter()
+        formatter.numberStyle = .DecimalStyle
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 2
+        return formatter
+    }()
+    
+    let dateFormatter: NSDateFormatter = {
+        let formatter = NSDateFormatter()
+        formatter.dateStyle = .MediumStyle
+        formatter.timeStyle = .NoStyle
+        return formatter
+    }()
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        nameTextField.text = item.name
+        serialTextField.text = item.serialNumber
+        valueTextField.text = numberFormatter.stringFromNumber(item.valueInDollars)
+        dateLabel.text = dateFormatter.stringFromDate(item.dateCreated)
+    }
 }
--- HomeBNR App/HomeBNR App/ItemsViewController.swift
+++ HomeBNR App/HomeBNR App/ItemsViewController.swift
@@ -80,4 +80,14 @@ class ItemsViewController: UITableViewController {
         tableView.rowHeight = UITableViewAutomaticDimension
         tableView.estimatedRowHeight = 65
     }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "ShowItem" {
+            if let row = tableView.indexPathForSelectedRow?.row {
+                let item = itemStore.allItems[row]
+                let detailViewController = segue.destinationViewController as! DetailViewController
+                detailViewController.item = item
+            }
+        }
+    }
 }
