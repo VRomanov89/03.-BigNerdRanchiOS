0e84bf5bd9a9ed8a9151d2b52744d292af87ffe2
Vladimir Romanov <vladromweb@gmail.com>
2016-08-19 15:04:57 -0700

    Added a "detail" page which will display the information about each item
    Added a "detail" page which will display the information about each item

---
+++ HomeBNR App/HomeBNR App/DetailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.swift
+//  HomeBNR App
+//
+//  Created by Volodymyr Romanov on 8/19/16.
+//  Copyright © 2016 Volodymyr Romanov. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+}
