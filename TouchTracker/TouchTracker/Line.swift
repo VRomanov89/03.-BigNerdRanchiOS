d135acdcda46e38b23ca41349ca0906cbd2efaaa
Vladimir Romanov <vladromweb@gmail.com>
2016-08-22 12:07:48 -0700

    TouchTracker - Line drawing on custom view
    TouchTracker - Line drawing on custom view

---
+++ TouchTracker/TouchTracker/Line.swift
@@ -0,0 +1,15 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by Volodymyr Romanov on 8/22/16.
+//  Copyright © 2016 Volodymyr Romanov. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+}
